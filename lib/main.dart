69710ae8344787e137674b22b6cf02e70660e6a6
vanshg395 <vansh@ivypods.com>
2022-07-06 18:07:51 +0530

    name changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const TApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TApp extends StatelessWidget {
+  const TApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
