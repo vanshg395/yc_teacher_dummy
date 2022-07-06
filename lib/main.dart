58d52370cdf41464275ee3786de6be879a13e56e
vanshg395 <vansh@ivypods.com>
2022-07-06 18:31:12 +0530

    student switchin g

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 // void main() => runApp(const TApp());
 
 class TApp extends StatelessWidget {
-  const TApp({Key? key}) : super(key: key);
+  const TApp({
+    required this.isStudent,
+    Key? key,
+  }) : super(key: key);
+
+  final ValueNotifier<bool> isStudent;
 
   // This widget is the root of your application.
   @override
@@ -21,13 +26,22 @@ class TApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        isStudent: isStudent,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({
+    Key? key,
+    required this.isStudent,
+    required this.title,
+  }) : super(key: key);
+
+  final ValueNotifier<bool> isStudent;
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,19 +59,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -95,18 +96,15 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextButton(
+              child: const Text('Switch to Student App'),
+              onPressed: () {
+                widget.isStudent.value = true;
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
